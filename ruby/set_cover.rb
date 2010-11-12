da3196f6e6e178766ff8bfe0929df318f412cf5e
Ben Nagy <github@ben.iagu.net>
2010-11-12 11:57:08 +0545

    Start work on set cover algorithms

--- ruby/set_cover.rb
+++ ruby/set_cover.rb
@@ -6,6 +6,7 @@
 require 'rubygems'
 require 'trollop'
 require File.dirname( __FILE__ ) + '/trace_codec'
+require File.dirname( __FILE__ ) + '/set_extensions'
 
 OPTS=Trollop::options do
     opt :file, "Trace DB file to use", :type=>:string, :default=>"ccov-traces.tch"
@@ -41,6 +42,56 @@ end
 tdb=TraceDB.new OPTS[:file], "re"
 puts (full=tdb.sample_fraction(1)).size
 puts (half=tdb.sample_fraction(0.5)).size
-puts full.sort_by {|k,v| Integer( v[:covered] ) }[-10..-1].map {|fn, hsh| "#{fn}-#{hsh[:covered].to_i}"}
-puts half.sort_by {|k,v| Integer( v[:covered] ) }[-10..-1].map {|fn, hsh| "#{fn}-#{hsh[:covered].to_i}"}
+
+def greedy_reduce( set_hash )
+    puts "Starting set #{set_hash.size}."
+    candidates=set_hash.sort_by {|k,v| Integer( v[:covered] ) }
+    minset=[]
+    coverage=Set.new
+    best_fn, best_hsh=candidates.shift
+    minset.push best_fn
+
+    # expand the starter set
+    best_set=Set.unpack( first[:trace] )
+    coverage=coverage.union( best_set )
+    puts "Starting set #{coverage} elems"
+
+    # strip elements from the candidates
+    # This is outside the loop so we only have to expand
+    # the sets to full size once.
+    candidates.each {|fn, hsh|
+        this_set=Set.unpack( hsh[:trace] )
+        hsh[:set]=(this_set - best_set)
+    }
+    candidates.delete_if {|fn, hsh| hsh[:set].empty? }
+    candidates=candidates.sort_by {|fn, hsh| hsh[:set].size }
+    best_fn, best_hsh==candidates.shift
+    minset.push best_fn
+    best_set=best_hsh[:set]
+    puts "Next best has #{best_set.size} elems left"
+    coverage=coverage.union( best_set )
+
+    # Now start the reduction loop, the Sets are expanded
+    puts "Starting reduction"
+    until candidates.empty?
+        candidates.each {|fn, hsh|
+            this_set=hsh[:set]
+            hsh[:set]=(this_set - best_set)
+        }
+        candidates.delete_if {|fn, hsh| hsh[:set].empty? }
+        candidates=candidates.sort_by {|fn, hsh| hsh[:set].size }
+        best_fn, best_hsh==candidates.shift
+        minset.push best_fn
+        best_set=best_hsh[:set]
+        coverage=coverage.union( best_set )
+        puts "C:#{candidates.size} M:#{minset.size} ccov:#{coverage.size}"
+    end
+    [minset, coverage]
+end
+
+puts "FULL: #{full.size} HALF: #{half.size}"
+minset, coverage=greedy_reduce( full )
+puts "FULL Minset #{minset.size}, covers #{coverage.size}".
+minset, coverage=greedy_reduce( half )
+puts "HALF Minset #{minset.size}, covers #{coverage.size}".
 
