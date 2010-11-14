f7662d66428111f915d095264fca67583d80034c
Ben Nagy <github@ben.iagu.net>
2010-11-14 11:40:35 +0545

    regressing, something broken

--- ruby/set_cover.rb
+++ ruby/set_cover.rb
@@ -181,7 +181,7 @@ end
 include Reductions
 tdb=TraceDB.new OPTS[:file], "re"
 full=tdb.sample_fraction(1)
-fraction=0.0625
+fraction=1/128.0
 samples=[]
 until fraction > 1 
     samples << tdb.sample_fraction( fraction )
